f0efa25390d0c1b77bc143f1b1d07bb4880b7315
lfrz <anonimus47@hotmail.com>
2024-03-02 13:56:04 -0500

    changing manager url to point to heroku api url

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -3,7 +3,7 @@ import { Manager, Socket } from "socket.io-client";
 let socket : Socket;
 
 export const connectToServer = (jwtToken: string) => {
-  const manager = new Manager("http://localhost:3000/socket.io/socket.io.js", {
+  const manager = new Manager("https://teslo-shop-lfrz-0f8e4e4abfe3.herokuapp.com/socket.io/socket.io.js", {
     extraHeaders: {
       authentication: jwtToken,
     },
